42fa61db59be87ec170c1b17aaeec6834fed9e17
cris-eci <cristian.pedraza-r@mail.escuelaing.edu.co>
2025-05-09 13:52:21 -0500

    feat: add initial scafolding

---
+++ src/main/java/edu/workshop/todo/TodoConsoleApplication.java
@@ -0,0 +1,13 @@
+package edu.workshop.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoConsoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/workshop/todo/TodoConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.workshop.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoConsoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
